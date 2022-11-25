33afc26b0fe8daa23724386263ec602a2643d26d
YashJain24 <75324135+YashJain24-chief@users.noreply.github.com>
2022-11-25 19:45:12 +0530

    listening to messageCreate event, interactionCreate event and registering slash commands

--- src/index.js
+++ src/index.js
@@ -1,10 +1,79 @@
 // basically client is a starting point of any bot
-import { Client } from "discord.js";
+import { Client, Routes } from "discord.js";
+
+// Rest is used for interacting with discord API's without much hassle
+import { REST } from "@discordjs/rest";
+
 import { config } from "dotenv";
 config();
 
-// we pass in intents because we want to receive events related to guillds and guild messages
-const client = new Client({ intents: ["Guilds", "GuildMessages"] });
+import open from "open";
 
-// Log in to Discord with your client's token and get your bot online!
-client.login(process.env.DISCORD_TOKEN);
+const BOT_TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = process.env.APP_ID;
+
+//creating a rest instance to start interacting with Discord API's
+const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
+
+// we pass in intents because we want to receive events related to guillds and guild messages
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
+
+//on basically listens to an event, Basically ready event is triggered whenever bot comes online
+
+client.on("ready", () => {
+  console.log("Bot is online!");
+});
+
+// messageCreate event is basically triggered whenever any message is sent on the server by someone
+//Enable MESSAGE CONTENT INTENT for your bot in developer portal
+client.on("messageCreate", (message) => {
+  console.log(
+    "onMessageCreate",
+    message.content,
+    message.client.user.id,
+    message.author.tag
+  );
+});
+
+//whenever we use a slash command, it triggers an interaction event and we have to listen to those interactions
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.commandName === "hey" && interaction.isChatInputCommand) {
+    await interaction.reply("Hello world");
+    console.log("interaction", interaction.user);
+  }
+  if (interaction.commandName === "verify" && interaction.isChatInputCommand) {
+    await interaction.reply("Opening a browser for your identity verification");
+    await open("https://www.google.com/");
+  }
+});
+
+const commands = [
+  {
+    name: "hey",
+    description: "Replies with Hello World!",
+  },
+  {
+    name: "verify",
+    description: "Opens a browser for verification",
+  },
+];
+
+async function main() {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    //registering/updating all our guild commands, recommended approach is to used global commands
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log("Successfully reloaded application (/) commands.");
+
+    // Log in to Discord with your client's token and get your bot online!
+    client.login(BOT_TOKEN);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
